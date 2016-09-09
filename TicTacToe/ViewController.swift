9d9c3971a39fd5c941528fa2aef620eae1600bdf
ayesha begum <ayeshabegum@learniosfreecoms-mac.local>
2016-09-09 11:37:48 -0400

    initial

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -1,25 +1,122 @@
-//
-//  ViewController.swift
-//  TicTacToe
-//
-//  Created by ayesha begum on 2016-09-07.
-//  Copyright © 2016 ayesha begum. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var winnerLabel: UILabel!
+    @IBOutlet var playAgainButton: UIButton!
+    
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        
+        activeGame = true
+        
+        activePlayer = 1
+        
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        for i in 1..<10 {
+            
+            if let button = view.viewWithTag(i) as? UIButton {
+                
+                button.setImage(nil, forState: [])
+                
+            }
+            
+            winnerLabel.hidden = true
+            playAgainButton.hidden = true
+            
+            winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
+            playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
+            
+        }
+        
+    }
+    
+    // 1 is noughts, 2 is crosses
+    
+    var activeGame = true
+    
+    var activePlayer = 1
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 - empty, 1 - noughts, 2 - crosses
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        let activePosition = sender.tag - 1
+        
+        if gameState[activePosition] == 0 && activeGame {
+            
+            gameState[activePosition] = activePlayer
+            
+            if activePlayer == 1 {
+                
+                sender.setImage(UIImage(named: "nought.png"), forState: [])
+                activePlayer = 2
+                
+                
+            } else {
+                
+                sender.setImage(UIImage(named: "cross.png"), forState: [])
+                activePlayer = 1
+                
+            }
+            
+            for combination in winningCombinations {
+                
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    // We have a winner!
+                    
+                    activeGame = false
+                    
+                    winnerLabel.hidden = false
+                    playAgainButton.hidden = false
+                    
+                    if gameState[combination[0]] == 1 {
+                        
+                        winnerLabel.text = "Noughts has won!"
+                        
+                    } else {
+                        
+                        winnerLabel.text = "Crosses has won!"
+                        
+                    }
+                   
+                    UIView.animateWithDuration(1, animations:  {
+                        
+                        self.winnerLabel.center = CGPoint(x: self.winnerLabel.center.x + 500, y: self.winnerLabel.center.y)
+                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x + 500, y: self.playAgainButton.center.y)
+                        
+                    })
+                    
+                  }
+               
+            }
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+                winnerLabel.hidden = true
+        
+        winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
+     //   playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
